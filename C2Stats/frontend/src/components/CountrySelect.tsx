042bce8433b4eaef7599b514a0f98ca0bfd5f627
Sergey Fadeev <fadeev@gmail.com>
2025-03-08 20:12:24 +0300

    extracted CountrySelect

---
+++ C2Stats/frontend/src/components/CountrySelect.tsx
@@ -0,0 +1,65 @@
+import { Select, Typography } from "antd";
+import { useEffect, useState } from "react";
+const { Text } = Typography;
+
+export interface CountryDatum {
+    code: string;
+    name: string;
+    value: number;
+}
+
+export interface CountrySelectorProps {
+    type: string;
+    date: Date;
+    country?: string;
+    onChange: (value?: string) => void;
+}
+
+export default ({ type, date, country, onChange }: CountrySelectorProps) => {
+
+    const [countries, setCountries] = useState<CountryDatum[] | null>(null);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const year = date?.getFullYear();
+
+        if (year) {
+            fetch(`/api/wod/countries?type=${type}&year=${year}`)
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Failed to fetch data');
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    setCountries(data);
+                })
+                .catch(err => setError('Error fetching data'));
+        }
+        else {
+            setCountries(null);
+        }
+    }, [type, date?.getFullYear()]);
+
+    return (<>
+        {countries ? (
+            <Select
+                value={country}
+                allowClear={true}
+                showSearch={true}
+                filterOption={true}
+                optionFilterProp='label'
+                style={{ width: 220 }}
+                options={countries.map(x => { return { value: x.code, label: x.name, count: x.value }; })}
+                optionRender={(option) => (
+                    <>
+                        {option.data.label}
+                        <Text style={{ float: 'right' }} type="secondary">{option.data.count}</Text>
+                    </>
+                )}
+                onSelect={(_, country) => onChange(country.value)}
+                onClear={() => onChange(undefined)}
+            />
+        ) : null}
+    </>);
+};
--- C2Stats/frontend/src/pages/Home.tsx
+++ C2Stats/frontend/src/pages/Home.tsx
@@ -1,44 +1,15 @@
-import { Card, DatePicker, Segmented, Select, Space, Typography } from 'antd';
+import { Card, DatePicker, Segmented, Space } from 'antd';
 import dayjs from 'dayjs';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import CountrySelect from '../components/CountrySelect';
 import WodCalendarData from '../components/WodCalendarData';
 import WodDayBarData from '../components/WodDayBarData';
-const { Text } = Typography;
-
-export interface CountryDatum {
-    code: string;
-    name: string;
-    value: number;
-}
 
 export default () => {
 
     const [date, setDate] = useState<Date | null>(new Date());
     const [type, setType] = useState<string | null>('rowerg');
-    const [country, setCountry] = useState<string | null>();
-    const [countries, setCountries] = useState<CountryDatum[] | null>(null);
-    const [error, setError] = useState<string | null>(null);
-
-    useEffect(() => {
-        const year = date?.getFullYear();
-
-        if (year) {
-            fetch(`/api/wod/countries?type=${type}&year=${year}`)
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('Failed to fetch data');
-                    }
-                    return response.json();
-                })
-                .then(data => {
-                    setCountries(data);
-                })
-                .catch(err => setError('Error fetching data'));
-        }
-        else {
-            setCountries(null);
-        }
-    }, [type, date?.getFullYear()]);
+    const [country, setCountry] = useState<string | undefined>();
 
     return (<>
 
@@ -60,25 +31,14 @@ export default () => {
                 onChange={(value) => setType(value)}
             />
 
-            {countries ? (
-                <Select
-                    value={country}
-                    allowClear={true}
-                    showSearch={true}
-                    filterOption={true}
-                    optionFilterProp='label'
-                    style={{ width: 220 }}
-                    options={countries.map(x => { return { value: x.code, label: x.name, count: x.value }; })}
-                    optionRender={(option) => (
-                        <>
-                            {option.data.label}
-                            <Text style={{ float: 'right' }} type="secondary">{option.data.count}</Text>
-                        </>
-                    )}
-                    onSelect={(value, country) => setCountry(country.value)}
-                    onClear={() => setCountry(null)}
+            {(date && type) ? (<>
+                <CountrySelect
+                    type={type}
+                    date={date}
+                    country={country}
+                    onChange={(value) => setCountry(value)}
                 />
-            ) : null}
+            </>) : null}
         </Space>
 
         {(date && type) ? (<>
